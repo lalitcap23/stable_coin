10f57486ea46559e1a034566807695f5fc03c8d8
lalitcap23 <lalitrajput232002@gmail.com>
2025-12-16 20:40:51 +0530

    first commit

---
+++ programs/stablecoin/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("2Wg97zfSSFgdcSyHcefD9dgafn5iXwjyQzpfzuX8tqjm");
+
+#[program]
+pub mod stablecoin {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
