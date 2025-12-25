914430b84f04f4dc25e8dbb1b5f86308242039b0
lalitcap23 <lalitrajput232002@gmail.com>
2025-12-25 23:56:42 +0530

    intialize/

--- programs/stablecoin/src/instructions/initialize_config.rs
+++ programs/stablecoin/src/instructions/initialize_config.rs
@@ -1,8 +1,8 @@
-use anchor_lang::{prelude::*, solana_program::{config, stake::config::Config}};
-use crate::constants::*;
+use anchor_lang::prelude::*;
+use crate::{constants::*, state::Config};
 
 #[derive(Accounts)]
-pub struct InitializeConfig {
+pub struct InitializeConfig<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 
@@ -13,10 +13,29 @@ pub struct InitializeConfig {
         bump,
         space = 8 + Config::INIT_SPACE, // Add space as needed
     )]
-    pub config_account:Account<'info,Config>,
+    pub config_account: Account<'info, Config>,
+    pub system_program: Program<'info, System>,
 }
 
-pub imp<'info> InitializeConfig<'info> {
-    Ok(())
+impl<'info> InitializeConfig<'info> {
+    pub fn initialize(
+        &mut self,
+        liquidity_threshold: u64,
+        liquidity_bonus: u64,
+        min_heath_factor: u64,
+        bumps: &InitializeConfigBumps,
+    ) -> Result<()> {
+        self.config_account.set_inner(Config {
+            authority: self.authority.key(),
+            mint_account: Pubkey::default(), // Set when mint is created
+            liquidity_threshold,
+            liquidity_bonus,
+            min_heath_factor,
+            bump: bumps.config_account,
+            bump_mint_acount: 0, // Set when mint is created
+        });
+        
+        Ok(())
+    }
 }
   
